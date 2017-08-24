0f3b9067b80336704c9771066604c393f381e0e7
John Gamboa <vaulttech@gmail.com>
2017-08-24 18:30:39 +0200

    Almost finishing Gersen class (as well as its Dataset)
     Changes to be committed:
    	modified:   README.md
    	modified:   gersen.py

--- utils/gersen.py
+++ utils/gersen.py
@@ -1,19 +1,61 @@
+import os
+import collections
 from tflearn.data_utils import to_categorical
 
 class Gersen(object):
-    def __init__(self, train_validate_split, test_split):
-        train = Dataset()
-        validate = Dataset()
-        test = Dataset()
+    def __init__(self, train_validate_split, test_split, use_defaults=False,
+                    shuffle=True):
+        self.dataset_name = 'GerSEN: Dataset with sentiment-annotated sentences'
+        self.dataset_descriptions = 'The dataset consists of sentiment ' \
+                    'annotated sentences.'
+        self.dataset_path = os.path.join(utils.data_root_directory, 'gersen')
 
-class Dataset(object):
-    def __init__(self, use_defaults=False, shuffle=True):
+        if use_defaults:
+            self.initialize_defaults(self)
+        else:
+            self.load_anew(self, train_validate_split, test_split)
+
+    def initialize_defaults(self):
         pass
 
-    def load(self, shuffle=True):
+    def load_anew(self, train_validate_split, test_split, shuffle):
+        all_data = load_all_data(self.dataset_path)
+
+        # if shuffle:
+        #   all_data = ...
+
+        # First we take the test data away
+        total_length = len(all_data)
+        test_length = int(total_length * test_split)
+        train_validate_data, test_data = all_data[:-test_length],\
+                                         all_data[-test_length:]
+
+        # Then we split the training/validation data
+        train_validate_length = len(train_validate_data)
+        train_length = int(train_validate_length * train_validate_split)
+        train_data, validate_data = train_validate_data[:train_length], \
+                                    train_validate_data[train_length:]
+
+        # Create vocabulary
+
+        self.train = DataSet(train_data, vocab, shuffle)
+        self.validate = DataSet(validate_data, vocab, shuffle)
+        self.test = DataSet(test_data, vocab, shuffle)
+
+    def load_all_data(self, path):
         pass
 
-    def next_batch(self, format='one_hot', rescale=None, pad=None,
+class DataSet(object):
+    def __init__(self, data, vocab, shuffle=True):
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+        self.vocab_w2i = vocab[0]
+        self.vocab_i2w = vocab[1]
+        self.datafile = None
+        self.Batch = collections.namedtuple('Batch', ['s1', 's2', 'sim'])
+
+    def next_batch(self, batch_size, seq_begin=False, seq_end=False,
+                   format='one_hot', rescale=None, pad=None,
                    return_sequence_lengths=False):
         # format: either 'one_hot' or 'numerical'
         # rescale: if format is 'numerical', then this should be a tuple
@@ -22,7 +64,7 @@ class Dataset(object):
         # x, y = ...
 
         if (format == 'one_hot'):
-            data = to_categorical(y, nb_classes=3)
+            y = to_categorical(y, nb_classes=3)
 
         if (rescale is not None):
             pass
@@ -33,19 +75,10 @@ class Dataset(object):
         if (return_sequence_lengths):
             pass
 
-        pass
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
 
-    def open(self):
-        pass
-
-    def close(self):
-        pass
-
-    def seq2i(self):
-        pass
-
-    def i2seq(self):
-        pass
 
 if __name__ == '__main__':
     g = Gersen()
