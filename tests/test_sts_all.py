12e7c271324a68098a7b3e6094f1b85d3d28c178
Ayushman Dash <a_dash@informatik.uni-kl.de>
2017-08-26 08:27:32 +0200

    [minor] added an option for metadata creation on the new_vocabulary

--- tests/test_sts_all.py
+++ tests/test_sts_all.py
@@ -1,4 +1,12 @@
 from utils import STSAll
+from nose.tools import assert_equal
+from nose.tools import assert_not_equal
+from nose.tools import assert_raises
+from nose.tools import raises
+
+
+def test_load_dataset():
+    assert_equal(STSAll().dataset_name, 'Semantic Text Similarity - All')
 
 def test_load_dataset():
     assert STSAll().dataset_name == 'Semantic Text Similarity - All'
