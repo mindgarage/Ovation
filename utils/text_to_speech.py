028d61562dd383539dc00ff619db4d942b2c4f34
John Gamboa <vaulttech@gmail.com>
2017-09-07 01:52:33 +0200

    Add speech2text and text2speech simple scripts
     Changes to be committed:
    	new file:   speech_to_text.py
    	new file:   text_to_speech.py

---
+++ utils/text_to_speech.py
@@ -0,0 +1,12 @@
+import pyttsx3
+
+if __name__ == '__main__':
+    # Initializes the engine
+    engine = pyttsx3.init()
+
+    # Says somethings
+    engine.say('I like coconut.')
+
+    # Produce audio
+    engine.runAndWait()
+
