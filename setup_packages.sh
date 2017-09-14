8078aaa44ca465064b3b9b90c2bfe36a069dc66f
Christoph Bauer <c.bauer@insiders-technologies.de>
2017-09-14 11:24:32 +0200

    missing comment hash in setup_packages.sh

--- setup_packages.sh
+++ setup_packages.sh
@@ -2,7 +2,7 @@
 pip install git+https://github.com/tflearn/tflearn.git
 
 
-Download the data and Models of NLTK and Spacy
+# Download the data and Models of NLTK and Spacy
 python -m nltk.downloader all
 python -m spacy download en_core_web_md
 python -m spacy download de_core_news_md
