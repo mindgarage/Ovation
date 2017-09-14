a032b10b8a165b92f32a481a60de24ca3390f604
Ayushman Dash <dash.ayushman.99@gmail.com>
2017-09-14 04:46:42 +0200

    minor add

--- setup_packages.sh
+++ setup_packages.sh
@@ -1,3 +1,8 @@
+# Install the bleeding edge version of TFLearn
+pip install git+https://github.com/tflearn/tflearn.git
+
+
+Download the data and Models of NLTK and Spacy
 python -m nltk.downloader all
 python -m spacy download en_core_web_md
 python -m spacy download de_core_news_md
